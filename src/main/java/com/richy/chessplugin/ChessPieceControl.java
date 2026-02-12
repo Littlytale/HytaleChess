c82bc3f55ca2528cd661e411058c39f0768c6395
Richy <103146442+Littlytale@users.noreply.github.com>
2026-02-12 11:49:32 +0100

    everything and nothing

---
+++ src/main/java/com/richy/chessplugin/ChessPieceControl.java
@@ -0,0 +1,82 @@
+package com.richy.chessplugin;
+
+import com.hypixel.hytale.codec.builder.BuilderCodec;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.math.vector.Vector3d;
+import com.hypixel.hytale.protocol.BlockPosition;
+import com.hypixel.hytale.protocol.InteractionType;
+import com.hypixel.hytale.server.core.entity.InteractionContext;
+import com.hypixel.hytale.server.core.entity.UUIDComponent;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import com.hypixel.hytale.server.npc.entities.NPCEntity;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.Nonnull;
+
+
+class ChessPieceControl extends SimpleInstantInteraction {
+
+    public static final BuilderCodec<ChessPieceControl> CODEC =
+            BuilderCodec.builder(ChessPieceControl.class, ChessPieceControl::new, SimpleInstantInteraction.CODEC).build();
+
+    private static final Map<UUID, Ref<EntityStore>> selectedEntity = new ConcurrentHashMap<>();
+
+
+    @Override
+    protected void firstRun(@Nonnull InteractionType interactionType,
+                            @Nonnull InteractionContext interactionContext,
+                            @Nonnull CooldownHandler cooldownHandler) {
+
+        // Holt sich Spieler und die UUID vom Spieler
+        Ref<EntityStore> playerRef = interactionContext.getOwningEntity();
+        UUID playerUUID = playerRef.getStore().getComponent(playerRef, UUIDComponent.getComponentType()).getUuid();
+
+        // Holt sich den TargetEntity ...
+        Ref<EntityStore> targetEntityRef = interactionContext.getTargetEntity();
+        // ... oder den TargetBlock
+        BlockPosition targetBlockPos = interactionContext.getTargetBlock();
+
+        // Speichert Entity
+        if (targetEntityRef != null ) {
+            if (interactionType.getValue() == InteractionType.Secondary.getValue()) {
+
+                selectedEntity.put(playerUUID, targetEntityRef);
+
+                return;
+            } else if (interactionType.getValue() == InteractionType.Primary.getValue()) {
+                Ref<EntityStore> currentEntity = selectedEntity.get(playerUUID);
+                if (!currentEntity.isValid()) {
+                    selectedEntity.remove(playerUUID);
+                    return;
+                }
+                NPCEntity entityComponent = selectedEntity.get(playerUUID).getStore().getComponent(currentEntity, NPCEntity.getComponentType());
+
+                entityComponent.getRole().setMarkedTarget("LockedTarget", targetEntityRef);
+                entityComponent.getRole().getStateSupport().setState(currentEntity, "Combat", null, currentEntity.getStore());
+
+            }
+        }
+
+        // Setzt den LeashPoint vom Entity über den Block der angeklickt wurde
+        if (targetBlockPos != null && selectedEntity.get(playerUUID) != null) {
+            Ref<EntityStore> currentEntity = selectedEntity.get(playerUUID);
+            if (!currentEntity.isValid()) {
+                selectedEntity.remove(playerUUID);
+                return;
+            }
+            NPCEntity entityComponent = selectedEntity.get(playerUUID).getStore().getComponent(currentEntity, NPCEntity.getComponentType());
+
+            entityComponent.setLeashPoint(new Vector3d(targetBlockPos.x + 0.5, targetBlockPos.y, targetBlockPos.z + 0.5));
+
+            entityComponent.getRole().getStateSupport().setState(selectedEntity.get(playerUUID), "ReturnHome", null, selectedEntity.get(playerUUID).getStore());
+
+            //selectedEntity.remove(playerUUID);
+
+            return;
+        }
+    }
+}
---
+++ src/main/java/com/richy/chessplugin/ChessPlugin.java
@@ -0,0 +1,29 @@
+package com.richy.chessplugin;
+
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.Interaction;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import java.util.logging.Level;
+
+import javax.annotation.Nonnull;
+
+
+public class ChessPlugin extends JavaPlugin {
+
+    public ChessPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        getLogger().at(Level.INFO).log("ExamplePlugin setup...");
+
+        this.getCodecRegistry(Interaction.CODEC)
+                .register("ChessPieceControl", ChessPieceControl.class, ChessPieceControl.CODEC);
+    }
+
+    @Override
+    protected void shutdown() {
+        getLogger().at(Level.INFO).log("Shutdown ExamplePlugin...");
+    }
+}
